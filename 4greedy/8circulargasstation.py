f570ddb9b6f92c7bf6bfa8f1d170280b8f6c1209
MOHITEhere <matharva655@gmail.com>
2026-08-07 12:26:24 +0530

    updating circular gas station

--- 4greedy/8circulargasstation.py
+++ 4greedy/8circulargasstation.py
@@ -1,25 +1,3 @@
-# Problem: Circular Gas Station
-
-# You are given two arrays:
-
-# gas[i] → amount of gas available at station i
-# cost[i] → gas required to travel from station i to (i+1)
-
-# 👉 The stations are arranged in a circular manner.
-
-# 🎯 Goal
-
-# 👉 Find the starting gas station index from where you can:
-
-# travel the entire circle once
-# without running out of gas
-
-# 👉 If not possible, return -1
-
-# 🧾 Example
-
-# Input:
-
 # gas  = [1, 2, 3, 4, 5]
 # cost = [3, 4, 5, 1, 2]
 # 🔍 Explanation
@@ -41,25 +19,24 @@
 
 
 class Gas:
-    def gas_station(self,gas,cost):
-        if sum(gas)<sum(cost):
+    def circular_gas_station(self,gas,cost):
+        if sum(cost)>sum(gas):
             return -1 
-
+            
+        ans=0
         start=0
-        tank=0
-
+        
         for i in range(len(gas)):
-            tank+=gas[i]-cost[i]
-
-        if tank<0:
-            start=i+1
-            tank=0
-
+            ans+=gas[i]-cost[i]
+            if ans<0:
+               start=i+1
+               ans=0
+                
         return start
-
-
-gas=list(map(int,input().split()))
-cost=list(map(int,input().split()))
+                
+            
+gas  = [1, 2, 3, 4, 5]
+cost = [3, 4, 5, 1, 2]
 ans=Gas()
-result=ans.gas_station(gas,cost)
+result=ans.circular_gas_station(gas,cost)
 print(result)
